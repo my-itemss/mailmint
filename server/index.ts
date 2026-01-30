be0cc7e06be6213125fc83a7b88153f83496ebef
arijiiiitttt🍉 <arijit123roy098@gmail.com>
2026-01-30 15:48:01 +0530

    index.ts updated

--- server/index.ts
+++ server/index.ts
@@ -6,23 +6,17 @@ import mailbox from "./routes/mailbox";
 import email from "./routes/email";
 import incoming from "./routes/incoming";
 
-const startServer = async () => {
-  await connectDB();
+await connectDB();
 
-  const app = new Hono();
+const app = new Hono();
 
-  app.route("/mailbox", mailbox);
-  app.route("/emails", email);
-  app.route("/incoming", incoming);
+app.route("/mailbox", mailbox);
+app.route("/emails", email);
+app.route("/incoming", incoming);
 
-  const port = Number(process.env.PORT) || 3000;
+serve({
+  fetch: app.fetch,
+  port: Number(process.env.PORT) || 3000,
+});
 
-  serve({
-    fetch: app.fetch,
-    port,
-  });
-
-  console.log(`Server running successfully on the port ${port}`);
-};
-
-startServer();
+console.log("Server running successfully");
